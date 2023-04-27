a379501115b3d44c88a00fef01fad2ccfbdf8f84
nikolamaric00 <dzonaa223@gmail.com>
2023-04-27 10:50:18 +0200

    added a blog page

---
+++ pages/blog/index.jsx
@@ -0,0 +1,9 @@
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  return (
+    <div className="flex justify-center items-center w-screen min-h-screen">
+      test
+    </div>
+  );
+}
